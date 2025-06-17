141d2989f2dcc79f08b8ceec8d111935ad2bf485
iajaygehlot24 <starajgehlot@gmail.com>
2025-06-17 08:50:52 +0530

    Update strings.py

--- TechVJ/strings.py
+++ TechVJ/strings.py
@@ -28,6 +28,3 @@ https://t.me/c/xxxx/101 - 120
 
 __note that space in between doesn't matter__"""
        
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
