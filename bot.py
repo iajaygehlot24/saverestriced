85ebfa259e1094cf195e00a92d7ac7b0a1e27e89
iajaygehlot24 <starajgehlot@gmail.com>
2025-06-17 08:52:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,6 +28,3 @@ class Bot(Client):
 
 Bot().run()
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
